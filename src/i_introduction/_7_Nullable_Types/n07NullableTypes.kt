b56a0091b24e1b680003791e6a1a315e966badde
Henrick Deschamps <1220932+hnrck@users.noreply.github.com>
2018-04-08 18:02:46 +0200

    exercise i.7

--- src/i_introduction/_7_Nullable_Types/n07NullableTypes.kt
+++ src/i_introduction/_7_Nullable_Types/n07NullableTypes.kt
@@ -1,26 +1,10 @@
 package i_introduction._7_Nullable_Types
 
-import util.TODO
-import util.doc7
-
-fun test() {
-    val s: String = "this variable cannot store null references"
-    val q: String? = null
-
-    if (q != null) q.length      // you have to check to dereference
-    val i: Int? = q?.length      // null
-    val j: Int = q?.length ?: 0  // 0
+fun todoTask7(client: Client?, message: String?, mailer: Mailer) {
+    if ((client?.personalInfo?.email == null) || (message == null)) return
+    val email: String = client.personalInfo.email
+    mailer.sendMessage(email, message)
 }
-
-fun todoTask7(client: Client?, message: String?, mailer: Mailer): Nothing = TODO(
-    """
-        Task 7.
-        Rewrite JavaCode7.sendMessageToClient in Kotlin, using only one 'if' expression.
-        Declarations of Client, PersonalInfo and Mailer are given below.
-    """,
-    documentation = doc7(),
-    references = { JavaCode7().sendMessageToClient(client, message, mailer) }
-)
 
 fun sendMessageToClient(
         client: Client?, message: String?, mailer: Mailer
@@ -28,8 +12,8 @@ fun sendMessageToClient(
     todoTask7(client, message, mailer)
 }
 
-class Client (val personalInfo: PersonalInfo?)
-class PersonalInfo (val email: String?)
+class Client(val personalInfo: PersonalInfo?)
+class PersonalInfo(val email: String?)
 
 interface Mailer {
     fun sendMessage(email: String, message: String)
